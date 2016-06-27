a32afe8b7628bff86998e7b39796c30e5f15d4cf
Rival Dwi Anggriyan Putra <rivaldwi3@gmail.com>
2016-06-27 05:52:21 -0700

    <commit rifqi>

--- Tugas1/src/tugas1/Tugas1.java
+++ Tugas1/src/tugas1/Tugas1.java
@@ -13,7 +13,7 @@ public class Tugas1 {
    
     public static void main(String[] args) {
         int t = 10;
-        String a = "sempak";
+        String a = "hahah";
         for(int i=0; i<t; i++){
             for(int j=0; j<t; j++){
                 if(i==2 || j==2){
